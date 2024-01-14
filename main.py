8335c3ef973cd1e11801d896fa4ce4a644bda0fd
Vinh Le <lethanhvinh95@gmail.com>
2024-01-14 11:03:52 +0200

    preserve conversations

--- main.py
+++ main.py
@@ -1,25 +1,43 @@
 from dotenv import load_dotenv
 import os
 
-from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import ChatOpenAI
 from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory
 
 load_dotenv()
 open_api_key = os.getenv("OPENAI_API_KEY")
 
 chat = ChatOpenAI()
 
+"""
+- memory_key: the key that will be used to store the memory in the memory object. 
+              It needs to be similar to the input variable name in the prompt template (ChatPromptTemplate).
+- returnMessages: true makes the memory return a list of chat messages instead of a string.
+"""
+memory = ConversationBufferMemory(memory_key="messages", return_messages=True)
+
 prompt = ChatPromptTemplate(
-  input_variables=["content"],
+  # This property specifies the input variables that will be passed to the prompt.
+  # In this case we have 2:
+  # - Content: what the user says to the chatbot
+  # - Messages: the chat history, for the chatbot to be conversational
+  input_variables=["content", "messages"],
   messages=[
-    HumanMessagePromptTemplate.from_template("{content}")
+    HumanMessagePromptTemplate.from_template("{content}"),
+    MessagesPlaceholder("messages"),
   ]
 )
 
+# Form the main chain
 chain = LLMChain(
   llm=chat,
   prompt=prompt,
+  # https://python.langchain.com/docs/modules/memory/adding_memory
+  memory=memory,
+  # Enable this option for better visibility on different phases of the chain (Entering, Finished)
+  verbose=True
 )
 
 while True:
